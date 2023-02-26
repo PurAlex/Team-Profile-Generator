ba3112b961a1dc0d7f15d60809dae5e3c7ae277f
Alexander <nicky-nico-1998@outlook.com>
2023-02-26 14:25:23 +0000

    questions class added

--- index.js
+++ index.js
@@ -10,6 +10,124 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+class Questions {
+    constructor() {
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+    }
 
+    teamChoices(){
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+                name: "teamMember",
+            },
+        ])
+        .then(({ teamMember }) => {
+            if (teamMember === "Engineer") {
+                this.engineerChoice();
+            } else if (teamMember === "Intern"){
+                this.internChoice();
+            } else if (teamMember === "I don't want to add any more team members"){
+                this.endChoices();
+            };
+        });
+    }
+
+    managerChoice() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the team manager's name?",
+                    name: "name",
+
+                },
+                {
+                    type: "input",
+                    message: "What is the team manager's id?",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "What is the team manager's email?",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "What is the team manager's office number?",
+                    name: "office",
+                },
+            ])
+            .then((response) => {
+                this.teamChoices();
+            });
+    }
+
+    engineerChoice() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your engineer's name?",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's id?",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's email?",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "What is your engineer's Github username?",
+                    name: "github",
+                }
+            ])
+            .then((response) => {
+                this.teamChoices();
+            });
+    }
+
+    internChoice() {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is your intern's name?",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's id?",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's email?",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "What is your intern's school?",
+                    name: "school",
+                },
+            ])
+            .then((response) => {
+                this.teamChoices();
+            });
+    }
+
+    endChoices(){
+        console.log("\nThank you!");
+    }
+}
+
+const questions = new Questions();
+questions.managerChoice();
